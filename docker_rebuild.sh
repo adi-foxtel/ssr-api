585cff9ae26a5cec74f811bd7955ebab19a55413
Adi Saric <adi.saric@foxtel.com.au>
2022-11-04 15:55:59 +1100

    change name to ssr

--- docker_rebuild.sh
+++ docker_rebuild.sh
@@ -2,10 +2,10 @@ echo START
 cd docker
 docker-compose down
 cd ..
-docker rmi db-api
+docker rmi ssr-api
 docker build . -t ssr-api -f docker/Dockerfile #--no-cache
 cd docker
 ./docker_compose.sh
 cd ..
 echo END
-docker logs -f docker_db-api_1
+docker logs -f docker_ssr-api_1
