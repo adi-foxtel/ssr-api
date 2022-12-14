3004648cb2db457718f2f2497fbde936eacb0092
Adi Saric <adi.saric@foxtel.com.au>
2022-12-14 16:22:18 +1100

    harmonic NMX

--- scripts/apifast.py
+++ scripts/apifast.py
@@ -20,15 +20,11 @@ import uvicorn
 from typing import Union
 
 from asp_clearscrambled import *
-from asp_harmonic import *
+from asp_nmx_api import *
 
 
 
-#from scripts import xmppapilib, configlib
-#import sleekxmpp
-#import scripts.iq3 as iq3
 from typing import Optional
-#from scripts.iq3 import util
 from fastapi_utils.tasks import repeat_every
 
 user = 'foxtel'
@@ -71,51 +67,17 @@ def get_versions():
     ver = "v0.3"
     return {"clearscrambled version": str(ver)}
 
-@app.get("/platform/manage/v1/harmonic/proswitch/realtime_status")
-def get_harmonic():
-    response =  harmonic_get_state()
+########################          SSR API         ##################################
+
+@app.get("/platform/manage/picture/scrambled/ssr/getdevices")
+def get_devices():
+    response =  nmx_get_devices()
     return JSONResponse(status_code=200, content=response)
-
-
-@app.get("/platform/manage/v1/harmonic/proswitch/cached_status")
-def get_harmonic():
-    response =  harmonic_get_state()
-    return JSONResponse(status_code=200, content=response)
-
-
-
-@app.get("/platform/manage/v1/harmonic/proswitch/getctrlmode")
-def get_harmonic():
-    return {"mode":"manual"}
-    response =  harmonic_get_state()
-
-@app.put("/platform/manage/v1/harmonic/proswitch/setctrlmode")
-async def post_harmonic(info : Request):
-    print("put") 
-    req_info = await info.json()
-    print(req_info) 
-    return req_info
-
-@app.post("/harmonic/api")
-async def post_harmonic(info : Request):
-    req_info = await info.json()
-    if req_info["command"] == "get_state":
-        response =  harmonic_get_state(req_info["command"])
-        return JSONResponse(status_code=200, content=response, default=str)
-    elif req_info["command"] == "set_state":    
-        response =  harmonic_set_state(req_info)
-        return JSONResponse(status_code=200, content=response)
-    elif req_info["command"] == "set_mode":    
-        response =  harmonic_set_mode(req_info)
-        return JSONResponse(status_code=200, content=response)
-
 
 @app.get("/platform/manage/picture/scrambled/ssr/gettsstreams")
 def get_clearscrambled():
     response =  SSR_get_streams()
     return JSONResponse(status_code=200, content=response)
-
-
 
 @app.get("/platform/manage/picture/scrambled/ssr/gettservicedetail/{item_id}")
 def get_service_details(item_id):
@@ -132,10 +94,41 @@ async def request_post(info : Request):
         response =  SSR_current_streams(req_info["command"],req_info["stream"],None,None)
         return JSONResponse(status_code=200, content=response)
 
-@app.get("/xmpp")
-def request_xmpp():
-    response =  "not implemented" 
-    return JSONResponse(200, response)
+########################          NMX API         ##################################
+
+@app.get("/platform/manage/picture/scrambled/nmx/getAccessToken")
+def get_nmx_token():
+    response =  nmx_get_devicesaccess_token()
+    return JSONResponse(status_code=200, content=response)
+
+@app.get("/platform/manage/picture/scrambled/nmx/getHarmonicDevices")
+def get_nmx_devices():
+    response = nmx_get_devices()
+    return JSONResponse(status_code=200, content=response)
+
+@app.get("/platform/manage/picture/scrambled/nmx/getServiceList")
+def get_nmx_servicelists():
+    response = nmx_get_service_lists()
+    return JSONResponse(status_code=200, content=response)
+
+
+@app.get("/platform/manage/picture/scrambled/nmx/getServiceGroups")
+def get_nmx_service_groups():
+    response = nmx_get_service_groups()
+    return JSONResponse(status_code=200, content=response)
+
+@app.get("/platform/manage/picture/scrambled/nmx/getPlanScramblingList")
+def get_nmx_scrambling_lists():
+    response = nmx_get_service_plans_scrambling_lists()
+    return JSONResponse(status_code=200, content=response)
+
+
+@app.post("/platform/manage/picture/scrambled/nmx/setClearScramble")
+async def request_post(info : Request):
+    req_info = await info.json()
+    response =  nmx_patch_channel(req_info["ID"], req_info["status"])
+    return JSONResponse(status_code=200, content=response)
+
 
 
 serverport = int(os.getenv('MYSQL_API_PORT', 9001))
---
+++ scripts/asp_nmx_api.py
@@ -0,0 +1,327 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+import requests
+from urllib3.exceptions import InsecureRequestWarning
+import os
+from datetime import datetime
+
+import mysql.connector as mysqlConnector
+
+class InitDataClass:
+    NMX = os.environ['NMX']
+    NMX_USER = os.environ['NMX_USER']
+    NMX_PASS = os.environ['NMX_PASS']
+
+
+obj = InitDataClass()
+
+def nmx_get_devicesaccess_token():
+
+    headers = {'content-type': 'application/json'}
+    url = f"https://{obj.NMX}/api/Domain/v2/AccessToken"
+
+    data = {
+        "Username": obj.NMX_USER,
+        "Password": obj.NMX_PASS
+    }
+
+    try:
+        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+        session = requests.Session()
+        session.verify = False
+
+        r = session.post( url, headers=headers, json=data)
+        data = json.loads(r.text)
+        #print(data)
+        return {"rezult": data}
+
+    except Exception as e:
+        print(e)
+        return {"rezult": "error"}
+        
+def nmx_get_devices():
+    
+    ret = nmx_get_devicesaccess_token()
+    if ret != 'error' :
+
+        access_token = ret['rezult']['access_token']
+        headers = {
+            'Accept': "application/json",
+            'Authorization': f"Bearer {access_token}"
+        }
+
+        url = f"https://{obj.NMX}/api/Topology/v2/Devices"
+        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+        session = requests.Session()
+        session.verify = False
+
+        try:
+            r = session.get( url, headers=headers)
+            data = json.loads(r.text)
+
+            harmonics=[]
+
+            for i in data:
+                if 'HardwareCategory' in i['DeviceInfo']:
+                    print("HardwareCategory exists: " + i['DeviceInfo']['HardwareCategory'])
+                    if i['DeviceInfo']['HardwareCategory'] == "Harmonic":
+                        harmonics.append({"ID": i['DeviceInfo']['ID'], "Name": i['DeviceInfo']['Name']})
+                else:
+                    continue
+
+            return harmonics
+
+        except Exception as e:
+            print(e)
+            return {"rezult": "error"}
+
+    return {"rezult": "error"}
+
+def nmx_patch_channel(id,status):
+
+    print(id)
+    print(status)
+    rezult={}
+
+    ret = nmx_get_devicesaccess_token()
+    if ret != 'error' :
+
+        access_token = ret['rezult']['access_token']
+        headers = {
+            'Content-Type': "application/json",
+            'Accept': "application/json",
+            'Authorization': f"Bearer {access_token}"
+        }
+
+        url = f"https://{obj.NMX}/api/Scrambling/v2/SCG/{id}"
+        print(url)
+        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+        session = requests.Session()
+        session.verify = False
+        
+        body={"status":status}
+        print(body)
+
+        try:
+           
+            r = session.patch(url, json=body, headers=headers)
+            data = json.loads(r.text)
+
+            rezult = data
+            print(rezult)
+            return rezult
+
+        except Exception as e:
+            print(e)
+            return {"rezult": "patch error"}
+
+    
+    return {"rezult": "token error"}  
+
+
+
+
+def nmx_get_service_plans_scrambling_lists():
+    
+    ret = nmx_get_devicesaccess_token()    
+    if ret != 'error' :
+        access_token = ret['rezult']['access_token']
+        headers = {
+            'Accept': "application/json",
+            'Authorization': f"Bearer {access_token}"
+        }
+
+        url = f"https://{obj.NMX}/api/Services/v2/ServicePlans"
+        requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+        session = requests.Session()
+        session.verify = False
+
+        try:
+            r = session.get( url, headers=headers)
+            data = json.loads(r.text)
+
+            scrambling=[]
+
+            ID = 'undefined'
+            active = False
+            for i in data:
+                if 'Active' in i:
+                    if i['Active'] == True:
+                        ID = i['ID']
+                        active = True
+                else:
+                    continue
+
+            print("ID: " + ID)
+            print("Active: " + str(active))
+
+            url = f"https://{obj.NMX}/api/Scrambling/v2/SCG?ServicePlanId={ID}"
+
+            requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+            session = requests.Session()
+            session.verify = False
+
+            try:
+                r = session.get( url, headers=headers)
+
+                data = json.loads(r.text)
+                scrambling = data
+
+                return scrambling
+
+
+            except Exception as e:
+                print(e)
+                return {"rezult": "error"}
+
+        except Exception as e:
+            print(e)
+            return {"rezult": "error"}
+
+    return {"rezult": "error"} 
+
+
+
+
+def nmx_get_service_lists():
+    
+    ret = nmx_get_devicesaccess_token()
+
+    if ret != 'error' :
+        
+        access_token = ret['rezult']['access_token']
+        headers = {
+            'Accept': "application/json",
+            'Authorization': f"Bearer {access_token}"
+        }
+
+        ret = nmx_get_devices()
+        print(ret)
+
+        if ret != 'error' :
+            
+            objectServices = []
+            
+            for i in ret:
+                ID = i['ID']
+                print(ID)
+
+                url = f"https://{obj.NMX}/api/Topology/v2/Devices/{ID}/ServiceList"
+                print(url)
+                requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+                session = requests.Session()
+                session.verify = False
+
+                try:
+                    r = session.get( url, headers=headers)
+                    data = json.loads(r.text)
+                    services=[]
+
+                    if len(data) > 0 :
+
+                        print("SERVICES")
+                        print(len(data))
+
+                        for s in data:
+                            services.append(s)
+
+                        objectServices.append({"ID": ID, "services": services})
+
+                except Exception as e:
+                    print(e)
+                    return {"rezult": "error"}
+
+        return(objectServices)
+
+    return {"rezult": "error"} 
+
+
+
+def nmx_get_service_groups():
+    
+    ret = nmx_get_devicesaccess_token()
+    print("nmx_get_service_groups")
+    myDict = {}
+
+    if ret != 'error' :
+        
+        access_token = ret['rezult']['access_token']
+        headers = {
+            'Accept': "application/json",
+            'Authorization': f"Bearer {access_token}"
+        }
+
+        ret = nmx_get_devices()
+        print(ret)
+
+        if ret != 'error' :
+            
+            ServiceGroups = []
+            ServiceGroupsSorted = [
+                [],[],[],[],[],[],[],[],[],[],
+                [],[],[],[],[],[],[],[],[],[],
+                [],[],[],[],[],[],[],[],[],[],
+                [],[],[],[],[],[],[],[],[],[]
+            ]
+            
+            for i in ret:
+                ID = i['ID']
+                print(ID)
+
+                url = f"https://{obj.NMX}/api/Topology/v2/Devices/{ID}/ServiceList"
+                requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+                session = requests.Session()
+                session.verify = False
+
+                try:
+                    r = session.get( url, headers=headers)
+                    data = json.loads(r.text)
+
+                    if len(data) > 0 :
+
+                        print("GROUPS")
+                        print(len(data))
+
+                        for s in data:
+                            tag = str(s["ServiceNumber"])
+                            if len(tag) > 3 :
+                                tag_service = tag[-3:]
+                                tag_group   = tag[:len(tag) - 3]
+                                #myDict[tag_group].append({"service": s})
+                                ServiceGroups.append({"group": int(tag_group), "service": s})
+
+                except Exception as e:
+                    print(e)
+                    return {"rezult": "error"}
+
+        plan = nmx_get_service_plans_scrambling_lists()
+
+        for i in ServiceGroups :
+            for p in plan:
+
+                if p['Name'] == str(i['service']['ServiceNumber']) :
+                    print("FOUND"+ p['Name'])
+                    print(p['Status'])
+                    i['service']['Status'] = p['Status']
+                    i['service']['ChannelId'] = p['ID']
+
+
+            ServiceGroupsSorted[int(i['group'])].append(i['service'])
+
+
+
+
+
+
+        return(ServiceGroupsSorted)
+
+    return {"rezult": "error"} 
+
+
+
+
+
+if __name__ == '__main__':
+    nmx_get_devicesaccess_token()
